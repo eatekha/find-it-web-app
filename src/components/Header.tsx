9157eaf8e9f317e67cc6283b0b079ecc47e99ef2
Eseosa Atekha <atekhaeseosa@gmail.com>
2025-04-15 19:08:48 -0400

    Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <a href="#default" className="logo">FindIT</a>
+                <div className="header-right">
+                    <a className="active" href="#home">Home</a>
+                    <a href="#contact">Contact</a>
+                    <a href="#about">About</a>
+                </div>
+        </div>
+      );
+}
+
+
+
+export default Header;
